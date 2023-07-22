48103461377a7a8975e7b96256070488e7c1ff79
Daniel Souza <daniell.oliveirra@gmail.com>
2023-07-22 16:50:46 -0300

    feat: Keycloak - Initial configuration

--- src/main/java/souza/oliveira/daniel/controller/ProposalController.java
+++ src/main/java/souza/oliveira/daniel/controller/ProposalController.java
@@ -1,10 +1,12 @@
 package souza.oliveira.daniel.controller;
 
+import io.quarkus.security.Authenticated;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import souza.oliveira.daniel.dto.ProposalDetailsDTO;
@@ -12,13 +14,16 @@ import souza.oliveira.daniel.service.ProposalService;
 
 @Path("/api/proposal")
 @ApplicationScoped
+@Authenticated
 public class ProposalController {
     private final Logger LOG = LoggerFactory.getLogger(ProposalController.class);
     private final ProposalService proposalService;
+    private final JsonWebToken jsonWebToken;
 
     @Inject
-    public ProposalController(ProposalService proposalService) {
+    public ProposalController(ProposalService proposalService, JsonWebToken jsonWebToken) {
         this.proposalService = proposalService;
+        this.jsonWebToken = jsonWebToken;
     }
 
     @GET
@@ -29,6 +34,7 @@ public class ProposalController {
     }
 
     @POST
+    @RolesAllowed({"proposal-customer"})
     public Response createNewProposal(ProposalDetailsDTO proposalDetails){
         LOG.info("-- Recebendo proposta de compra --");
 
@@ -38,6 +44,7 @@ public class ProposalController {
 
     @DELETE
     @Path("/{id}")
+    @RolesAllowed({"manager"})
     public Response removeProposal(@PathParam("id") long id){
         proposalService.removeProposal(id);
         return Response.ok().build();
